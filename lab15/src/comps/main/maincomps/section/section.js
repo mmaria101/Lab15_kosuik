f576c14bd1ae4527b998b154695f236dd1fa979f
mmaria101 <Mmariakk0505@mail.ru>
2025-05-09 00:16:34 +0300

    Lab15_v3

--- lab15/src/comps/main/maincomps/section/section.js
+++ lab15/src/comps/main/maincomps/section/section.js
@@ -5,7 +5,7 @@ function Section() {
     <div className="section">
     <img src="https://img.freepik.com/free-vector/hand-drawn-russian-victory-day-illustration_23-2148906093.jpg?semt=ais_hybrid&w=740"
     title="С днем победы!"
-    alt="С днем победы!"
+    alt="С днем победы!!"
     height="250px"
     weight="250px"
     />
